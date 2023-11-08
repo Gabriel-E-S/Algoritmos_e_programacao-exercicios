ec3f456d758ca4a9f9264b4a54e4889181fb871e
Gabriel-E-S <gabriel.e.santo007@gmail.com>
2023-11-07 22:23:14 -0300

    aprimorando o número 3

--- 2º Semestre - 1º Trimestre/Lista envolvendo arquivos/003/3)Versão em C.c
+++ 2º Semestre - 1º Trimestre/Lista envolvendo arquivos/003/3)Versão em C.c
@@ -31,7 +31,7 @@ void criandofiliais(){
       reg_filial f1[tam] ={
             {1,"Motor","Kombi","Volks",1900,2005,100,10},
             {2,"suco de laranja","Celta","Chevrolet",2000,2010,100,10},
-            {3,"Suco de maracuja","Civic","Honda",1999,2012,150,115}
+            {3,"Suco de maracuja","Civic","Honda",1999,2012,150,10}
 };
 
     reg_filial f2[tam] = {
@@ -44,8 +44,8 @@ void criandofiliais(){
     FILE *filial2 = fopen("Dados filial 2.bin","wb");
 
     if(filial1 != NULL && filial2 != NULL){
-        fwrite(f1,sizeof(f1),tam,filial1);
-        fwrite(f2,sizeof(f2),tam,filial2);
+        fwrite(f1,sizeof(reg_filial),tam,filial1);
+        fwrite(f2,sizeof(reg_filial),tam,filial2);
 
         fclose(filial1);
         fclose(filial2);
@@ -85,49 +85,47 @@ void criandovetmatriz(){
     }
 }
 
-void verificar_existencia(){
+   void verificar_existencia() {
+    FILE *filial2 = fopen("Dados filial 2.bin", "rb");
+    FILE *matriz = fopen("Dados da matriz.bin", "r+b");
 
-    bool existe;
-    reg_matriz copiam;
-    reg_filial copia;
+    if (filial2 != NULL && matriz != NULL) {
+        reg_filial copia;
 
-    FILE *filial2 = fopen("Dados filial 2.bin","rb");
-    FILE *matriz = fopen("Dados da matriz.bin","ab");
+        while (fread(&copia, sizeof(reg_filial), 1, filial2) == 1) {
+            bool existe = false;
+            reg_matriz copiam;
 
-    if(filial2 != NULL && matriz != NULL){
+            // Procura e atualiza os registros no arquivo
 
-        while(!feof(filial2)){
-            existe = false;
-
-            fread(&copia,sizeof(reg_filial),1,filial2);
-            rewind(matriz);
-
-            while(fread(&copiam,sizeof(reg_matriz),1,matriz) == 1){
-                    if(copia.codigo == copiam.codigo){
-                        copiam.qtd_vendida = copiam.qtd_vendida + copia.qtd;
-                        copiam.valor_total = copiam.valor_total + copia.qtd * copia.preco;
-                        existe = true;
-                        fwrite(&copiam,sizeof(reg_matriz),1,matriz);
-
-                    }
-
+            while (fread(&copiam, sizeof(reg_matriz), 1, matriz) == 1) {
+                if (copia.codigo == copiam.codigo) {
+                    copiam.qtd_vendida += copia.qtd;
+                    copiam.valor_total += copia.qtd * copia.preco;
+                    existe = true;
+                    fseek(matriz, -sizeof(reg_matriz), SEEK_CUR);
+                    fwrite(&copiam, sizeof(reg_matriz), 1, matriz);
+                    break;
+                }
             }
 
-            if(!existe){
-                fseek(matriz,qtdmatriz*sizeof(reg_matriz),SEEK_SET);
+            // Se não encontrou o código, adiciona um novo registro no arquivo
+            if (!existe) {
+                fseek(matriz, 0, SEEK_END);
                 copiam.codigo = copia.codigo;
                 copiam.qtd_vendida = copia.qtd;
-                copiam.valor_total = copia.preco * copia.qtd;
-                fwrite(&copiam,sizeof(reg_matriz),1,matriz);
-                qtdmatriz+=1;
+                copiam.valor_total = copia.qtd * copia.preco;
+                fwrite(&copiam, sizeof(reg_matriz), 1, matriz);
             }
+
+            rewind(matriz); // Retorna o arquivo pro início
         }
+
         fclose(filial2);
         fclose(matriz);
-}else{
-    printf("Erro ao abrir novamente 2\n");
+    }
 }
-}
+
 
 void imprimirfiliais(int opc){
 
@@ -188,12 +186,11 @@ void imprimirmatriz(){
     if(matriz != NULL){
 
     printf("\n\n***DADOS DA MATRIZ\n\n");
+    printf("CÓDIGO\tQUANTIDADE VENDIDA\tVALOR TOTAL\n\n");
 
     while(fread(&copia,sizeof(reg_matriz),1,matriz) == 1 ){
 
-            printf("Código: %d\n",copia.codigo);
-            printf("Quantidade vendida: %d",copia.qtd_vendida);
-            printf("Valor total: %.2f",copia.valor_total);
+            printf("  %d\t\t%d\t\t  %.2f\n",copia.codigo,copia.qtd_vendida,copia.valor_total);
 
     }
 
@@ -204,13 +201,84 @@ void imprimirmatriz(){
 }
 }
 
+int maiorvenda(){
+
+    int maior,ondemaior = 1;
+    reg_matriz copia;
+
+        FILE *matriz = fopen("Dados da matriz.bin","rb");
+
+        if(matriz != NULL){
+
+        fread(&copia,sizeof(reg_matriz),1,matriz);
+
+        maior = copia.qtd_vendida;
+
+        while(fread(&copia,sizeof(reg_matriz),1,matriz)==1){
+            if(copia.qtd_vendida > maior){
+                maior = copia.qtd_vendida;
+                ondemaior = copia.codigo;
+            }
+        }
+
+        printf("\n\nA maior venda foi da peça com código: %d\n\n",ondemaior);
+
+        return ondemaior;
+        }
+        else{
+            printf("Erro !");
+        }
+
+        fclose(matriz);
+}
+
+void acharpecamaisvendida(int codigo){
+
+    bool achou = false;
+    reg_filial copia;
+
+    FILE *filial1 = fopen("Dados filial 1.bin","rb");
+    FILE *filial2 = fopen("Dados filial 2.bin","rb");
+
+    if(filial1 != NULL && filial2 != NULL){
+
+        while(fread(&copia,sizeof(reg_filial),1,filial1) == 1){
+                if(codigo == copia.codigo){
+                    printf("peca mais vendida : %s\n",copia.descricao);
+                    printf("modelo de carro: %s\n",copia.modelo);
+                    achou = true;
+                }
+        }
+        if(!achou){
+
+             while(fread(&copia,sizeof(reg_filial),1,filial2) == 1){
+                if(codigo == copia.codigo){
+                    printf("peca mais vendida : %s\n",copia.descricao);
+                    printf("modelo de carro: %s\n",copia.modelo);
+                    achou = true;
+                }
+        }
+
+        }
+        fclose(filial1);
+        fclose(filial2);
+
+
+    }
+    else{
+        printf("Erro!\n");
+    }
+
+
+
+}
 
 
 
 
 
 int main(){
-
+    int y;
     setlocale(LC_ALL,"");
     criandofiliais();
     criandovetmatriz();
@@ -220,6 +288,8 @@ int main(){
 
     imprimirmatriz();
 
+    y = maiorvenda();
+    acharpecamaisvendida(y);
 
 
 
